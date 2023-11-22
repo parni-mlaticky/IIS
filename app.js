a353035319e3aa562981d1ce7752f062795ceea7
Petr Kolouch <kolouch.petr4@email.cz>
2023-11-22 13:02:04 +0100

    Added method-override

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const methodOverride = require("method-override");
 require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 app.use("/", routes);
 
